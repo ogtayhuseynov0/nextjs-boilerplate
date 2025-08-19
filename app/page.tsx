3538384b4ff6bbc5d27b80a9894f6395d8d6ff31
ogtayhuseynov0 <ogtayhuseynov0@gmail.com>
2025-08-19 14:14:55 -0400

    init

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="bg-red-200">
+      <Button>asda</Button>
+    </div>
+  );
+}
---
+++ store/init.store.ts
@@ -0,0 +1,8 @@
+import { create } from 'zustand'
+
+interface InitStete {
+}
+
+const useBearStore = create<InitStete>()((set) => ({
+
+}))
